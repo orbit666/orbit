085e8eb1a73a69dcea392b70d250b6cf92e3f2a3
orbit666 <63085968+orbit666@users.noreply.github.com>
2023-03-01 19:33:13 +0800

    lightsail.sh

---
+++ lightsail.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+sudo apt update
+sudo apt upgrade
+echo root:dingning. |sudo chpasswd root
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+apt-get update && apt-get install vim nano sysstat vnstat curl -y
+curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+mkdir -p ~/docker/fb/config ~/docker/fb/myfiles
+docker run -d --name fb \
+  --restart=unless-stopped \
+  -e PUID=$UID \
+  -e PGID=$GID \
+  -e WEB_PORT=8082 \
+  -e FB_AUTH_SERVER_ADDR="127.0.0.1" \
+  -p 8082:8082 \
+  -v ~/docker/fb/config:/config \
+  -v /:/myfiles \
+  --mount type=tmpfs,destination=/tmp \
+  80x86/filebrowser:2.9.4-amd64
+bash -c 'echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf'
+bash -c 'echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf'
+sysctl -p

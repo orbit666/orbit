1d229361c5a366aa8b47fe6e130af9d2c711f333
orbit666 <63085968+orbit666@users.noreply.github.com>
2023-03-01 20:49:31 +0800

    Update seed.sh

--- seed.sh
+++ seed.sh
@@ -2,7 +2,9 @@
 echo root:orbit |sudo chpasswd root
 sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
 sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+
 apt-get update && apt-get install vim nano sysstat vnstat curl -y
+
 mkdir -p ~/docker/fb/config ~/docker/fb/myfiles
 docker run -d --name fb \
   --restart=unless-stopped \
@@ -15,9 +17,7 @@ docker run -d --name fb \
   -v /:/myfiles \
   --mount type=tmpfs,destination=/tmp \
   80x86/filebrowser:2.9.4-amd64
-bash -c 'echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf'
-bash -c 'echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf'
-sysctl -p
+  
 apt update -y &&
 apt upgrade -y &&
 apt install apparmor apparmor-utils -y &&
@@ -28,4 +28,5 @@ timedatectl set-timezone Asia/Shanghai &&
 mkdir -p /root/vertex &&
 chmod 777 /root/vertex &&
 docker run -d --name vertex --restart unless-stopped --network host -v /root/vertex:/vertex -e TZ=Asia/Shanghai lswl/vertex:stable
+
 echo -e "y\n1\nn\nn\ny\n" | bash <(wget -qO- https://raw.githubusercontent.com/jerry048/Dedicated-Seedbox/main/Install.sh) orbit orbit 4096

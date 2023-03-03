117fe257d6332658706fb31e1dd88ee2ac8a5ba5
orbit666 <63085968+orbit666@users.noreply.github.com>
2023-03-03 19:20:12 +0800

    Update qrencode.sh

--- qrencode.sh
+++ qrencode.sh
@@ -1,3 +1,2 @@
 sudo apt-get install qrencode -y > /dev/null
-qrencode -t UTF8 https://tuapi.eees.cc/api.php?category=dongman&type=302 &&
 qrencode -t ASCIIi https://tuapi.eees.cc/api.php?category=dongman&type=302

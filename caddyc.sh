36da6ac65fbc316b60a2c6ac60a70f610d1b3a93
orbit666 <63085968+orbit666@users.noreply.github.com>
2023-03-03 12:35:18 +0800

    Update caddyc.sh

--- caddyc.sh
+++ caddyc.sh
@@ -1,1 +1,51 @@
+# Define the Caddyfile path and name
+caddyfile="/etc/caddy/Caddyfile"
+
+# Define the Caddyfile content
+caddyfile_content=":443, orbit1024.top
+tls /etc/letsencrypt/live/orbit1024.top/fullchain.pem /etc/letsencrypt/live/orbit1024.top/privkey.pem
+route {
+  forward_proxy {
+    basic_auth yourname pass
+    hide_ip
+    hide_via
+    probe_resistance
+  }
+  file_server {
+    root /var/www/html 
+  }
+}"
+
+# Write the Caddyfile content to the file
+echo "$caddyfile_content" > "$caddyfile"
+
+# Restart the Caddy server
+caddy fmt --overwrite /etc/caddy/Caddyfile
+caddy run --config /etc/caddy/Caddyfile
+
+
+echo "[Unit]
+Description=Caddy
+Documentation=https://caddyserver.com/docs/
+After=network.target network-online.target
+Requires=network-online.target
+[Service]
+Type=notify
+User=root
+Group=root
+ExecStart=/usr/bin/caddy run --environ --config /etc/caddy/Caddyfile
+ExecReload=/usr/bin/caddy reload --config /etc/caddy/Caddyfile
+TimeoutStopSec=5s
+LimitNOFILE=1048576
+LimitNPROC=512
+PrivateTmp=true
+ProtectSystem=full
+AmbientCapabilities=CAP_NET_BIND_SERVICE
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/naive.service
+
+systemctl daemon-reload
+systemctl enable naive
+systemctl start naive
+systemctl status naive
 

5c54aa23a6f7c5727ce49d8ea8c336670031b7bd
orbit666 <63085968+orbit666@users.noreply.github.com>
2023-03-03 19:22:27 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,8 +4,9 @@
 echo "请选择一项操作:"
 echo -e "\033[1m\033[34m1. 1.jerry脚本,自定义选项\033[0m"
 echo -e "\033[1m\033[34m1. 2.yabs vps测试脚本\033[0m"
-echo -e "\033[1m\033[34m1. 3.退出\033[0m"
-echo -e "\033[1m\033[34m1. 4.看看色图\033[0m"
+echo -e "\033[1m\033[34m1. 3.看看色图\033[0m"
+echo -e "\033[1m\033[34m1. 4.退出\033[0m"
+
 
 # 循环等待用户输入
 while true; do
@@ -30,6 +31,9 @@ case "$option" in
         curl -sL yabs.sh | bash
         ;;
     3)
+        echo"色图来喽"
+        bash <(wget -qO- https://raw.githubusercontent.com/orbit666/orbit/master/qrencode.sh)
+    4)
         echo "退出脚本。"
         exit 0
         ;;
